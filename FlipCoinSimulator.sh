1f4c77fc46049df09819dd1059ee2c52dd39f4b3
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 16:40:39 +0530

    Win time Check

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 
-var=$((RANDOM%2))
+count=0
+while [ $count -ne 21 ]
+do
+	var=$((RANDOM%2))
+	#echo $var
 
-if [ $var -eq 1 ]
-then
-	echo "Head"
-else 
-	echo "Tail"
-fi
+	if [[ $var -eq 1 ]]
+	then
+		echo "Head win the first"
+		
+	else
+
+		echo "tail win the first"
+
+	fi
+
+	((count++))
+done
