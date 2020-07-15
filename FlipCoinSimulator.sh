126c792ad0f35d25393866cbf2b4b9c47d6310d2
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 16:44:35 +0530

    Count head or tail

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,10 +1,34 @@
 #!/bin/bash -x
+#!/bin/bash -x
 
-var=$((RANDOM%2))
+headCount=0
+tailCount=0
+while [ true ]
+do
+	var=$((RANDOM%2))
+	#echo $var
 
-if [ $var -eq 1 ]
-then
-	echo "Head"
-else 
-	echo "Tail"
-fi
+	if [[ $var -eq 1 ]]
+	then
+		((headCount++))
+
+		if [[ $headCount -eq 21 ]]
+		then
+			echo "Head win the first"
+			break
+		fi
+	else
+		((tailCount++))
+
+		if [[ $tailCount -eq 21 ]]
+		then
+			echo "tail win the first"
+			break
+		fi
+
+	fi
+
+
+done 
+echo "Head Count is $headCount"
+echo "Tail Count is $tailCount"
